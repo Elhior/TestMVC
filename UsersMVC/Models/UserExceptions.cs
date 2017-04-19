a8f27530322a51cd805c5b66e444d5e65c055754
Elhior <Arhangelc@rambler.ru>
2017-04-19 11:58:49 +0300

    class for tracking server validation exceptions added

---
+++ UsersMVC/Models/UserExceptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UsersMVC.Models
+{
+    public class UserExceptions
+    {
+        public bool isEmpty;
+        public string LoginException
+        {
+            get;
+            set;
+        }
+                     
+        public string EmailException
+        {
+            get;
+            set;
+        }
+
+        public string PasswordException
+        {
+            get;
+            set;
+        }
+    }
+}
